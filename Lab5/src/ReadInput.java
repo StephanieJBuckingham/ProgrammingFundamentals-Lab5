64c7a7c996cc230dc5404fc32c2e90b3d0b6111c
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-08-10 01:08:12 -0400

    Try to Fix Bug

--- Lab5/src/ReadInput.java
+++ Lab5/src/ReadInput.java
@@ -7,8 +7,9 @@ public class ReadInput {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		// read in csv file input.csv
-		File file = new File(System.getProperty("user.dir/input.csv"));
-		Scanner scan = new Scanner(file);
+		String file = "input.csv";
+		File csv = new File("./src/input.csv");
+		Scanner scan = new Scanner(csv);
 
 		// print out title Maximum Values
 		System.out.println("Maximum Values");
@@ -22,7 +23,7 @@ public class ReadInput {
 		while (scan.hasNext()) {
 			row = scan.nextLine();
 			String[] data = row.split(",");
-			
+
 			// separate data into rows and determine max
 			for (int i = 0; i < data.length; i++) {
 				if (Integer.parseInt(data[i]) > max)
@@ -32,9 +33,9 @@ public class ReadInput {
 			System.out.println("MAX " + row + ": " + max);
 			count++;
 
-			}
-			// close scanner to prevent resource leaks
-			scan.close();
-
 		}
+		// close scanner to prevent resource leaks
+		scan.close();
+
 	}
+}
