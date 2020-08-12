9649172ad6c4efe2b8642b24f25290945303324d
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-08-12 07:06:13 -0400

    Fix Bug

--- Lab5/src/ReadInput.java
+++ Lab5/src/ReadInput.java
@@ -15,27 +15,27 @@ public class ReadInput {
 		System.out.println("Maximum Values");
 
 		// initialize variables
-		String row = "";
 		int max = 0;
 		int count = 1;
 
 		// create loop to scan in csv values
 		while (scan.hasNext()) {
-			row = scan.nextLine();
-			String[] data = row.split(",");
-
-			// separate data into rows and determine max
-			for (int i = 0; i < data.length; i++) {
-				if (Integer.parseInt(data[i]) > max)
-					max = Integer.parseInt(data[i]);
-			}
+			
 			// print out max values in each row
-			System.out.println("MAX " + row + ": " + max);
-			count++;
+			System.out.println("MAX " + count++ + ": " + max(scan.nextLine().split(",")));
 
 		}
 		// close scanner to prevent resource leaks
 		scan.close();
-
+		}
+		
+	private static String max(String[] values) {
+		String max = values[0];
+	      for (String value : values) {
+	         if (Integer.valueOf(value) > Integer.valueOf(max)) {
+	            max = value;
+	         }
+	      }
+	      return max;
 	}
-}
+	}
